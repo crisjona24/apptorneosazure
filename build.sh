027fb34c3df310c5040e3d659f0d3e1a06151934
Cristobal.Rios <cristobal.rios@unl.edu.ec>
2022-12-13 13:11:51 -0500

    Modificando para Render con build

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
+# exit on error*-
 set -o errexit
 
 #poetry install
-pip install -r requirements.txt 
-
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
+pip install --upgrade pippip install --force-reinstall -U setuptools
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
